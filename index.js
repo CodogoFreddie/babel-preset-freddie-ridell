812aec9b04edb257f9ebdccf16281922e418d86b
Freddie Ridell <freddie@codogo.io>
2018-04-26 18:42:25 +0000

    basic idea

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-module.exports = {
+const targets = {
+	browsers: "> 1%",
+	node: "current",
+};
 
-}
+module.exports = {
+	presets: [
+		"stage-3",
+		"react",
+		[
+			"env",
+			{
+				targets,
+				modules: process.env.BABEL_OPTIONS.contains("webpack")
+					? false
+					: "umd",
+			},
+		],
+	],
+	plugins: [
+		"transform-class-properties",
+		[
+			"transform-runtime",
+			{
+				polyfill: false,
+				regenerator: true,
+			},
+		],
+	],
+};
