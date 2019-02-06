1324dd9a46c90e09fe5c99fc03b0cddbb53561dd
Freddie Ridell <contact@freddieridell.com>
2019-02-06 17:28:49 +0000

    wip

--- index.js
+++ index.js
@@ -3,35 +3,64 @@ const targets = {
 	node: "current",
 };
 
-module.exports = {
+/*
+ * options:
+ * ramdaUseEs: should ramda use its es imports
+ * emotion: should we use emotions
+ * modules: how should env package its modules
+ *
+ * env vars:
+ * MINIFY = true | false
+ * NODE_ENV = "production" | "develop" | "test" 
+ */
+
+module.exports = (_, { ramdaUseEs, emotion, modules }) => ({
+	comments: !process.env.NODE_ENV === "production" && process.env.MINIFY,
 	presets: [
+		"@babel/react",
+		"@babel/flow",
+		process.env.NODE_ENV === "production" && process.env.MINIFY && "minify",
 		[
-			"env",
+			"@babel/env",
 			{
 				targets,
-				modules: (process.env.BABEL_OPTIONS || "").includes("webpack")
-					? false
-					: "commonjs",
+				modules,
 			},
 		],
-		"stage-3",
-		"react",
-	],
+	].filter(Boolean),
 	plugins: [
-		"transform-class-properties",
-		"transform-decorators-legacy",
+		"preval",
+		emotion && "emotion",
+		["ramda", { useEs: Boolean(ramdaUseEs) }],
 		[
-			"ramda",
+			"@babel/plugin-transform-runtime",
 			{
-				useES: false,
-			},
-		],
-		[
-			"transform-runtime",
-			{
-				polyfill: false,
+				corejs: false,
+				helpers: true,
 				regenerator: true,
+				useESModules: false,
 			},
 		],
-	],
-};
+	].filter(Boolean),
+	//presets: [
+	//"stage-3",
+	//"react",
+	//],
+	//plugins: [
+	//"transform-class-properties",
+	//"transform-decorators-legacy",
+	//[
+	//"ramda",
+	//{
+	//useES: false,
+	//},
+	//],
+	//[
+	//"transform-runtime",
+	//{
+	//polyfill: false,
+	//regenerator: true,
+	//},
+	//],
+	//],
+});
