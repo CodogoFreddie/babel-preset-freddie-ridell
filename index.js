c129a58e1a34674fc8398f1282baa9a82c367e4a
Freddie Ridell <contact@freddieridell.com>
2019-02-08 15:37:17 +0000

    added help text

--- index.js
+++ index.js
@@ -5,62 +5,59 @@ const targets = {
 
 /*
  * options:
- * ramdaUseEs: should ramda use its es imports
- * emotion: should we use emotions
- * modules: how should env package its modules
+ *     ramdaUseEs: should ramda use its es imports
+ *     emotion: should we use emotions
+ *     modules: how should env package its modules
  *
  * env vars:
- * MINIFY = true | false
- * NODE_ENV = "production" | "develop" | "test" 
+ *     MINIFY = true | false
+ *     NODE_ENV = "production" | "develop" | "test" 
  */
 
-module.exports = (_, { ramdaUseEs, emotion, modules }) => ({
-	comments: !process.env.NODE_ENV === "production" && process.env.MINIFY,
-	presets: [
-		"@babel/react",
-		"@babel/flow",
-		process.env.NODE_ENV === "production" && process.env.MINIFY && "minify",
-		[
-			"@babel/env",
-			{
-				targets,
-				modules,
-			},
-		],
-	].filter(Boolean),
-	plugins: [
-		"preval",
-		emotion && "emotion",
-		["ramda", { useEs: Boolean(ramdaUseEs) }],
-		[
-			"@babel/plugin-transform-runtime",
-			{
-				corejs: false,
-				helpers: true,
-				regenerator: true,
-				useESModules: false,
-			},
-		],
-	].filter(Boolean),
-	//presets: [
-	//"stage-3",
-	//"react",
-	//],
-	//plugins: [
-	//"transform-class-properties",
-	//"transform-decorators-legacy",
-	//[
-	//"ramda",
-	//{
-	//useES: false,
-	//},
-	//],
-	//[
-	//"transform-runtime",
-	//{
-	//polyfill: false,
-	//regenerator: true,
-	//},
-	//],
-	//],
-});
+module.exports = (_, { ramdaUseEs, emotion, modules }) => {
+	if (process.env.BABEL_HELP) {
+		console.log(`
+@freddieridell/babel-preset:
+    options:
+        ramdaUseEs: should ramda use its es imports
+        emotion: should we use emotions
+        modules: how should env package its modules
+
+    env vars:
+        MINIFY = true | false
+        NODE_ENV = "production" | "develop" | "test" 
+		`);
+	}
+
+	return {
+		comments: !process.env.NODE_ENV === "production" && process.env.MINIFY,
+		presets: [
+			"@babel/react",
+			"@babel/flow",
+			process.env.NODE_ENV === "production" &&
+				process.env.MINIFY &&
+				"minify",
+			[
+				"@babel/env",
+				{
+					targets,
+					modules,
+				},
+			],
+		].filter(Boolean),
+		plugins: [
+			"preval",
+			emotion && "emotion",
+			["ramda", { useEs: Boolean(ramdaUseEs) }],
+			[
+				"@babel/plugin-transform-runtime",
+				{
+					corejs: false,
+					helpers: true,
+					regenerator: true,
+					useESModules: false,
+				},
+			],
+		].filter(Boolean),
+	};
+};
